a29cdf51b9c2a9003457f394c97d8964497874a5
markjt031 <markjt031@gmail.com>
2023-07-09 14:33:58 -0400

    created carousel

--- src/components/ProjectCarousel.js
+++ src/components/ProjectCarousel.js
@@ -16,19 +16,20 @@ const ProjectCarousel = ({data}) => {
   }
   console.log(data.data.images)
   return (
-    <div class="max-w-screen-lg mx-auto mt-20 dark:text-zinc-200">
+    <div class="max-w-screen-lg mx-auto dark:text-zinc-200">
       <div class="flex">
-        <div id="previous" class="text-5xl flex justify-center items-center cursor-pointer p-2 relative -top-8" onClick={scrollLeft}>
+        <div id="previous" class="text-5xl flex text-slate-400 justify-center items-center cursor-pointer p-2 relative -top-8" onClick={scrollLeft}>
           <FontAwesomeIcon icon={faAngleLeft}/>
         </div>
-        <div id='carousel' class="scroll-smooth flex-1 mt-4 flex w-full gap-4 pb-5 snap-x overflow-x-auto">
+        <div id='carousel' class="scroll-smooth mt-4 flex w-[320px] min-h-[200px] md:w-[500px] gap-4 pb-5 snap-x overflow-x-auto mx-auto">
           {data.data.images.map((image, i)=>{
-            return <div className="min-w-[100%] md:min-w-[50%]" key={i}>
-              <div style={{backgroundImage: `url(${image})`}} class="h-80 w-full snap-center rounded-md bg-cover bg-center shadow-md shadow-black/20 "></div>
+            return <div className="min-w-[100%]"key={i}>
+              <img src={image}/>
+              <div class="object-contain h-full w-full snap-center rounded-md bg-center shadow-md shadow-black/20 "></div>
             </div>
           })}
         </div>
-        <div id="next" class="text-5xl flex justify-center items-center cursor-pointer p-2 relative -top-8" onClick={scrollRight}>
+        <div id="next" class="text-5xl flex  text-slate-400 justify-center items-center cursor-pointer p-2 relative -top-8" onClick={scrollRight}>
           <FontAwesomeIcon icon={faAngleRight}/>
       </div>
     </div>
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -6,7 +6,12 @@ const Project = () => {
   let {state}=useLocation()
   console.log(state)
   return (
-    <ProjectCarousel data={state}>Project</ProjectCarousel>
+    <>
+    <div className=" bg-stone-200 text-slate-800">
+      <h1 className="text-5xl text-center mt-6">{state.data.title}</h1>
+      <ProjectCarousel data={state}>Project</ProjectCarousel>
+    </div>
+    </>
   )
 }
 
